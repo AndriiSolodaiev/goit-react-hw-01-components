dfeba60a25fe1c52cf5909b68f01a48a1d345724
AndriiSolodaiev <andik05016@gmail.com>
2023-02-13 02:39:32 +0200

    add propType title

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -27,7 +27,8 @@ const Statistics = ({ title, stats }) => {
 export default Statistics;
 
 Statistics.propTypes = {
-  items: PropTypes.arrayOf(
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
     PropTypes.exact({
       id: PropTypes.string.isRequired,
       label: PropTypes.string.isRequired,
